ea9a10470c82ba315339e5e71663ff6d379f18ec
rileyp2000 <rileyp@bishopireton.org>
2019-09-25 17:50:27 -0400

    doing calculations

--- matlab/c1_p2.m
+++ matlab/c1_p2.m
@@ -5,5 +5,21 @@
 clc
 clear
 
-dims = input("Insert the dimensions of cuboid LxWxH: ",'s')
-dim = split(dims,"x")
+dims = input("Insert the dimensions of cuboid LxWxH: ",'s');
+dim = split(dims,"x");
+
+x = str2double(dim(1));
+y = str2double(dim(2));
+z = str2double(dim(3));
+
+%volume = lxwxh
+vol = x * y * z;
+%surface area = 2xlxw + 2xlxh + 2xwxh
+sa = 2*x*y + 2*x*z + 2*y*z;
+
+%mass = density * vol
+density_iron = 7860;
+mass = density_iron * vol;
+
+
+
