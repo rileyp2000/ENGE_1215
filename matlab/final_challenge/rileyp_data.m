ea2e685758824e523ab78c0a506732c1c3d9f58e
rileyp2000 <rileyp@bishopireton.org>
2019-11-07 22:22:33 -0500

    done?

--- matlab/final_challenge/rileyp_data.m
+++ matlab/final_challenge/rileyp_data.m
@@ -2,6 +2,9 @@
 %11/6/19
 %This is the final matlab project with the swimsuit datset
 
+clear 
+clc
+
 % Import data from text file (Taken from the matlab import data functionality)
 
 %% Setup the Import Options
@@ -20,7 +23,6 @@ swimsuit_searches = readtable("/swimsuit.csv", opts);
 clear opts
 
 disp("Data Loaded");
-
 
 %This part of the program tracks the searches by month and creates a matrix to represent that data
 monthly_data = zeros(16,12);
@@ -43,7 +45,7 @@ for month = 1:12
 end
 
 %Plot the mean of each month on the graph along with a box and whisker plot for that data
-scatter(1:12,mean(monthly_data));
+scatter(1:12, mean(monthly_data));
 legend("Monthly Mean Search Result");
 hold on
 boxplot(monthly_data);
@@ -57,4 +59,59 @@ hold off
 
 
 %%Step 3
-%TODO: graph one month for all years, make bullshit prediction based on that
+%TODO: graph one month for all years, make prediction based on that
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+mon = input("Please input the month you are interested in looking at (i.e. 'january'): ", "s");
+range = input("How many years backward would you like to look? ");
+
+for i = 1:12
+    if strcmpi(months(i), mon) == 1
+        mon = i;
+    end
+end
+
+%Gets all values for the month in in the specified range
+r = find(swimsuit_searches.Month == mon);
+monthly_searches = swimsuit_searches(r,:);
+r = find(monthly_searches.Year > (2019-range));
+monthly_searches = monthly_searches(r,:);
+
+%Creates a LOBF for the searches in the month over the range
+x = monthly_searches.Year;
+y = monthly_searches.SearchInterest;
+trends = polyfit(x,y, 1);
+
+
+%Determines the trend in the data over the range based on how steep the slope of the LOBF is
+direction = "";
+severity = "";
+if trends(1) >= 0
+    direction = "up";
+else
+    direction = "down";
+end
+if abs(trends(1)) >= 1
+    severity = "significantly";
+else
+    severity = "a little";
+end
+
+fprintf("Over the last %i years, the general trend of search results in %s has gone %s %s\n", range, months(mon), direction, severity);
+
+%Logical conditionals to get the most recent search interst for the month
+m = swimsuit_searches.Month == mon;  
+yr = swimsuit_searches.Year == 2019;
+a = m == yr;                        
+b = m == 1;
+
+%Prints out the most recent number for the search frequency of the month requested
+fprintf("Search result frequency for %s of 2019: %i\n", months(mon), swimsuit_searches.SearchInterest(a & b));
+
+
+%Plots a graph of the search interest for the month in quesiton by year
+figure
+plot(x, y);
+xlabel("Year");
+ylabel("Search Interest");
+tit = sprintf("Yearly search interest for 'swimsuit' in %s",months(mon));
+title(tit);
